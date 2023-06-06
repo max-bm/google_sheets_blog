59044d2f01be74cd3bf19d93ef001bbc2cc8c8ba
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-06-06 09:31:19 +0100

    Learning.

--- infra/provider.tf
+++ infra/provider.tf
@@ -10,3 +10,13 @@ terraform {
     prefix = "terraform/blog"
   }
 }
+
+# provider "google" {
+#   # Configuration options
+#   scopes = ["https://www.googleapis.com/auth/drive",
+#     "https://www.googleapis.com/auth/bigquery",
+#     "https://www.googleapis.com/auth/cloud-platform",
+#   "https://www.googleapis.com/auth/userinfo.email"]
+#   project = var.project_id
+#   region  = var.region
+# }
