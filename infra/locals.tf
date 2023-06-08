7746a4ee350aa01f8f0315391f0d8bcba74cbac7
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-06-08 14:48:03 +0100

    Dataset needs name, not a map...

--- infra/locals.tf
+++ infra/locals.tf
@@ -11,15 +11,13 @@ locals {
   }
   table_list = [
     for ds in local.datasets : {
-      for tbl in ds.tables : "${ds.name}-${tbl.name}" => tbl
+      for tbl in ds.tables : "${ds.name}-${tbl.name}" => merge(tbl, { dataset_id = ds.name })
     }
   ]
-  tables = zipmap(
-    flatten([for item in local.table_list : keys(item)]),
-    flatten([for item in local.table_list : values(item)])
-  )
-  # tables = flatten([for dataset in local.bigquery.bigquery.datasets : [
-  #   for tbl, val in dataset.tables : merge(val, { dataset_id = dataset.name })
-  # ]])
-  # for_each = { for tbl in local.tables : "${tbl.dataset_id}-${tbl.name}" => tbl }
-}
+  tables = {
+    for tbl in flatten([
+      for ds in local.datasets : [
+        for key, val in ds.tables : merge(val, { dataset_id = ds.name})
+      ]
+    ]) : "${tbl.dataset_id}-${tbl.name}" => tbl
+  } 
