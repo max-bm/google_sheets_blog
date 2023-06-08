0bd25d951e36aff706a4cb52a941b9b6421c2bbd
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-06-08 13:58:06 +0100

    For each nonsense.

--- infra/locals.tf
+++ infra/locals.tf
@@ -9,11 +9,11 @@ locals {
   datasets = {
     for ds in local.bigquery.bigquery.datasets : ds.name => ds
   }
-  tables = {
-    for ds in local.datasets : {
+  tables = flatten({
+    for ds in local.datasets : ds.name => {
       for tbl in ds.tables : "${ds.name}-${tbl.name}" => tbl
-    } => ds
-  }
+    }
+  })
   # tables = flatten([for dataset in local.bigquery.bigquery.datasets : [
   #   for tbl, val in dataset.tables : merge(val, { dataset_id = dataset.name })
   # ]])
