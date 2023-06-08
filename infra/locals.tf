c4eeb13e3488ac66e843babc730d1273e1ea8c8e
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-06-08 14:54:54 +0100

    ...

--- infra/locals.tf
+++ infra/locals.tf
@@ -16,9 +16,14 @@ locals {
   ]
   tables = {
     for tbl in flatten([
-      for ds in local.datasets : [
-        for key, val in ds.tables : merge(val, { dataset_id = ds.name })
+      for ds_key, ds_val in local.datasets : [
+        for tbl_key, tbl_val in ds_val.tables : merge(tbl_val, { dataset_id = ds_val.name })
       ]
     ]) : "${tbl.dataset_id}-${tbl.name}" => tbl
   }
+
+  # tables = flatten([for dataset in local.bigquery.bigquery.datasets : [
+  #   for tbl, val in dataset.tables : merge(val, { dataset_id = dataset.name })
+  # ]])
+  # for_each = { for tbl in local.tables : "${tbl.dataset_id}-${tbl.name}" => tbl }
 }
