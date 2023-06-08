14d2f730a2515be094b7dcd9de1207ea17e386f6
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-06-08 14:48:40 +0100

    ...

--- infra/locals.tf
+++ infra/locals.tf
@@ -17,7 +17,8 @@ locals {
   tables = {
     for tbl in flatten([
       for ds in local.datasets : [
-        for key, val in ds.tables : merge(val, { dataset_id = ds.name})
+        for key, val in ds.tables : merge(val, { dataset_id = ds.name })
       ]
     ]) : "${tbl.dataset_id}-${tbl.name}" => tbl
-  } 
+  }
+}
