96fddf750254c47c3fc4bf2a84499346fd863cd0
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-06-08 17:50:53 +0100

    Merging sheets URI from yaml.

--- infra/locals.tf
+++ infra/locals.tf
@@ -15,10 +15,10 @@ locals {
         for tbl_inner in ds.tables : merge(
           tbl_inner,
           { dataset_id = ds.name },
-          { external_data_configuration = merge([
+          { external_data_configuration = merge(
             tbl_inner.external_data_configuration,
             { sheets_uris = local.project_config.sheets_uri }
-          ]) }
+          ) }
         )
       ]
     ]) : "${tbl.dataset_id}-${tbl.name}" => tbl
