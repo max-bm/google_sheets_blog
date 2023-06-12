2f89d93798ed42cbbd32ab281745edb44908c831
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-06-12 15:33:30 +0100

    limiting perms.

--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,13 @@ data "google_service_account" "sheets_access" {
   ]
 }
 
+data "google_service_account" "cloud_build" {
+  account_id = "${data.google_project.demo_project.number}@cloudbuild.gserviceaccount.com"
+  depends_on = [
+    resource.google_project_service.enable_apis
+  ]
+}
+
 resource "google_project_iam_custom_role" "sheets_access_roles" {
   role_id = "sheetsAccessRole"
   title   = "Sheets Access Role"
@@ -24,7 +31,7 @@ resource "google_project_iam_binding" "sheets_access_perms" {
   project = local.project_config.project_id
   role    = resource.google_project_iam_custom_role.sheets_access_roles
   members = [
-    data.google_service_account.sheets_access.name
+    data.google_service_account.sheets_access.member
   ]
 }
 
@@ -32,7 +39,7 @@ resource "google_service_account_iam_binding" "impersonate_sheets_access" {
   service_account_id = data.google_service_account.sheets_access.name
   role               = "roles/iam.serviceAccountTokenCreator"
   members = [
-    "serviceAccount:${data.google_project.demo_project.number}@cloudbuild.gserviceaccount.com"
+    google_service_account.cloud_build.member
   ]
 }
 
