7919bd90c6c42f060b1c24a163c0944972ac4d2d
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-06-12 14:39:54 +0100

    limiting perms.

--- infra/main.tf
+++ infra/main.tf
@@ -12,34 +12,34 @@ data "google_service_account" "sheets_access" {
   ]
 }
 
-resource "google_project_iam_custom_role" "sheets-access-roles" {
-  role_id = "sheetsAccessRole"
-  title   = "Sheets Access Role"
-  # permissions = [
+# resource "google_project_iam_custom_role" "sheets-access-roles" {
+#   role_id = "sheetsAccessRole"
+#   title   = "Sheets Access Role"
+#   permissions = [
 
-  # ]
-}
+#   ]
+# }
 
-resource "google_service_account_iam_binding" "impersonate_sheets_access" {
-  service_account_id = data.google_service_account.sheets_access.name
-  role               = "roles/iam.serviceAccountTokenCreator"
-  members = [
-    "serviceAccount:${data.google_project.demo_project.number}@cloudbuild.gserviceaccount.com"
-  ]
-}
+# resource "google_service_account_iam_binding" "impersonate_sheets_access" {
+#   service_account_id = data.google_service_account.sheets_access.name
+#   role               = "roles/iam.serviceAccountTokenCreator"
+#   members = [
+#     "serviceAccount:${data.google_project.demo_project.number}@cloudbuild.gserviceaccount.com"
+#   ]
+# }
 
-data "google_service_account_access_token" "gdrive" {
-  target_service_account = data.google_service_account.sheets_access.email
-  scopes = [
-    "https://www.googleapis.com/auth/drive",
-    # "https://www.googleapis.com/auth/cloud-platform",
-    # "https://www.googleapis.com/auth/userinfo.email"
-  ]
-  lifetime = "3600s"
-  depends_on = [
-    resource.google_service_account_iam_binding.impersonate_sheets_access
-  ]
-}
+# data "google_service_account_access_token" "gdrive" {
+#   target_service_account = data.google_service_account.sheets_access.email
+#   scopes = [
+#     "https://www.googleapis.com/auth/drive",
+#     # "https://www.googleapis.com/auth/cloud-platform",
+#     # "https://www.googleapis.com/auth/userinfo.email"
+#   ]
+#   lifetime = "3600s"
+#   depends_on = [
+#     resource.google_service_account_iam_binding.impersonate_sheets_access
+#   ]
+# }
 
 provider "google" {
   alias        = "impersonated"
